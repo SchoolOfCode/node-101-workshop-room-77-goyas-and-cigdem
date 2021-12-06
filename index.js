34df3fc2682888314c362419df36c28b18691fc0
CigdemOzturk <cigdemozturk_@hotmail.com>
2021-12-06 19:02:14 +0000

    Task 3 is done

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
-let myCollection = [
-    {
-      name: "School of Code mug",
-      count: 1,
-      whatILike: "It has my cute pixel character on it!"
-    },
-    {
-      name: "School of Code hat",
-      count: 2,
-      whatILike: "An often overlooked fashion accessory"
-    },
-    {
-      name: "School of Code pillow",
-      count: 1,
-      whatILike: "Eat. Sleep. Code. Repeat :)"
-    }
-  ];
-
+import myCollection from './collection.js';
 //   console.log(myCollection);
-
-const printArray = myCollection.map(describeItem);
 
 function describeItem(item){
     if(item.count == 1){
         console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike} `);
     }else{
-        console.log(`I have ${item.count} ${item.name}'s. Here's what I like about them: ${item.whatIlike}`);
+        console.log(`I have ${item.count} ${item.name}'s. Here's what I like about them: ${item.whatILike}`);
     }
 }
+
+myCollection.map(describeItem);
+
+function describeCollection(myArray){
+    for(let i = 0; i < myCollection.length; i++){
+        describeItem(myCollection[i]);
+    }
+}
+describeCollection(myCollection);
