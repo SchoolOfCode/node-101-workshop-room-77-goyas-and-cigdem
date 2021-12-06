23b67055f37920b12d5f626ffad73ea5a1ba577b
CigdemOzturk <cigdemozturk_@hotmail.com>
2021-12-06 19:28:29 +0000

    Bonus Task 1 is done

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import chalk from 'chalk';
 
 function describeItem(item){
     if(item.count == 1){
-        console.log(`I have a ${chalk.cyan(item.name)}. Here's what I like about it: ${chalk.green(item.whatILike)} `);
+        console.log(chalk.bold.bgRed(`I have a ${chalk.cyan(item.name)}. Here's what I like about it: ${chalk.green(item.whatILike)} `));
     }else{
-        console.log(`I have ${chalk.yellow(item.count)} ${chalk.cyan(item.name)}'s. Here's what I like about them: ${chalk.green(item.whatILike)}`);
+        console.log(chalk.bold.bgRed(`I have ${chalk.yellow(item.count)} ${chalk.cyan(item.name)}'s. Here's what I like about them: ${chalk.green(item.whatILike)}`));
     }
 }
 
